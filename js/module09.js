7b39742a429c9b0a05c1ed16c3e615988f782d13
MostlyHarmless <sja4201@gmail.com>
2021-05-31 13:15:15 -0700

    m09 done

--- js/module09.js
+++ js/module09.js
@@ -1,14 +1,32 @@
-var  mybutton = document.querySelector("button");
-mybutton.addEventListener("click", function(event) {
 
-
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
+// This is messy but I'm done dealing with it
+function clearing(){
+    setTimeout(clearAll, 5);
 }
 
-// Let us stop the propagation of events
+function clearAll(){
+    document.querySelectorAll('.dot').forEach(e => e.remove());
+}
 
+
+function clicked() {
+    console.log("clicked");
+    // from https://eloquentjavascript.net/15_event.html
+    let dot = document.createElement("div");
+    dot.className = "dot";
+    dot.style.left = (event.pageX - 4) + "px";
+    dot.style.top = (event.pageY - 4) + "px";
+    document.body.appendChild(dot);
+}
+
+addEventListener("click", clicked, false);
+clear = document.getElementById("clear");
+clear.addEventListener("click", clearing, false);
+
+
+
+// Let us stop the propagation of events
+/*
 event.stopPropagation();
   });
   addEventListener("click", function(event) {
@@ -18,3 +36,4 @@ event.stopPropagation();
     dot.style.top = (event.pageY - 4) + "px";
     document.body.appendChild(dot);
 });
+*/
